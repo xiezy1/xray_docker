563a49bc83ef7ceb75aab99efd441f561f471ddf
xiezy <2973912389@qq.com>
2023-10-25 17:41:47 +0800

    1

--- utils/index.js
+++ utils/index.js
@@ -24,7 +24,11 @@ function startXray(config) {
     stopXray();
     if (config) fs.writeFileSync("/usr/local/Xray/config.json", JSON.stringify(config), "utf-8");
     else console.log("使用默认config：/usr/local/Xray/config.json");
-    execs("/usr/local/Xray/xray", (err, so, se) => {});
+    execs("/usr/local/Xray/xray", (err, so, se) => {
+        console.log("err", err);
+        console.log("so", so);
+        console.log("se", se);
+    });
 }
 
 function getsubscriptList() {
